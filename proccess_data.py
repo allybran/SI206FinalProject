b2db4992e1de2d60d769cf1b28dc6b45488d8854
Lauren Starr <laurenstarr@Laurens-MacBook-Air-51.local>
2025-04-21 16:36:40 -0400

    working on writing combined meal info into  a text file

--- proccess_data.py
+++ proccess_data.py
@@ -1,10 +1,17 @@
+import sqlite3
 
+#connects to SQLite database
+def connect_db():
+    return sqlite3.connect("meals.db")
+
+#combine meal + related data and write to file
 def process_data():
-    conn = sqlite3.connect("meals.db")
+    conn = connect_db()
     cur = conn.cursor()
     cur.execute("""
-        SELECT Meals.name, Meals.rating, Nutrition.calories, Nutrition.fat_g, Nutrition.sugar_g, Nutrition.protein_g
+        SELECT Meals.name, Meals.rating, Recipes.popularity, Nutrition.calories, Nutrition.fat_g, Nutrition.sugar_g, Nutrition.protein_g
         FROM Meals
+        JOIN Recipes ON Meals.id = Recipes.meal_id
         JOIN Nutrition ON Meals.id = Nutrition.meal_id
     """)
     results = cur.fetchall()
@@ -12,4 +19,7 @@ def process_data():
 
     with open("output.txt", "w") as f:
         for r in results:
-            f.write(f"{r[0]} | Rating: {r[1]} | Calories: {r[2]} | Fat: {r[3]}g | Sugar: {r[4]}g | Protein: {r[5]}g\n")
+            f.write(f"{r[0]} | Rating: {r[1]} | Popularity: {r[2]} | Calories: {r[3]} | Fat: {r[4]}g | Sugar: {r[5]}g | Protein: {r[6]}g\n")
+
+if __name__ == "__main__":
+    process_data()
