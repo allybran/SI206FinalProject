20401f358b51e898a306b6c581ae51e80c339433
Ally Brangham and Lauren Starr <allybrangham@Allys-MacBook-Air-4.local>
2025-04-21 11:13:05 -0400

    minor edits

--- import requests.py
+++ import requests.py
@@ -2,14 +2,18 @@
 #Lauren Starr and Ally Brangham
 #Group Name: Ann Arbor Foodies
 
+<<<<<<< HEAD
 comment
 
 
+=======
+>>>>>>> 70c3813 (minor updates + comments)
 import sqlite3
 import json
 import requests
-import pandas as pd
-import seaborn as sns
+#import pandas as pd
+#import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 
 # We made an account to download a free API key from Nutritionix
@@ -49,7 +53,7 @@ def create_tables():
     conn.commit()
     conn.close()
 
-    # load meals from custom JSON 
+    # load meals from custom JSON file  
     #limits to 25 per run
 def load_meals_json(filename="yelp_data.json"):
     with open(filename, "r") as f:
@@ -60,21 +64,22 @@ def load_meals_json(filename="yelp_data.json"):
     for item in all_meals:
         if item.get("restaurant name") and item.get("Popular_dish") and isinstance(item.get("rating"), (int, float)):
             try:
+                #insert data into a "Meals" table
                 cur.execute("INSERT OR IGNORE INTO Meals (name, restaurant, rating) VALUES (?, ?, ?)", 
                             (item["Popular_dish"], item["restaurant name"], item["rating"]))
                 count += 1
-                if count >= 25:
-                    break
+                if count >= 25: #project requirement
+                    break 
             except:
                 continue
     conn.commit()
     conn.close()
     
 
-#searching nutrition info for each popular meal 
+#opening the api and storing data 
 def get_nutrition_facts(): 
     conn, cur = connect_db()
-    cur.execute("SELECT id, name FROM meals") 
+    cur.execute("SELECT id, name FROM meals") #SQL SELECT query
     meals = cur.fetchall()
     
     #inputting my personalized api keys
@@ -90,7 +95,8 @@ def get_nutrition_facts():
         try: 
             response =requests.post(url, headers=headers, json={"query":food_item})
             food = response.json()["foods"][0] #this is going to accept the first match of the search
-            cur.execute("""
+            #inserting nutrition info with SQL parameters
+            cur.execute(""" 
                 INSERT OR IGNORE INTO Nutrition (meal_id, calories, fat_grams, sugar_grams, protein_grams)
                 VALUES (?, ?, ?, ?, ?)
             """,(meal_id, food["nf_calories"], food["nf_total_fat"], food["nf_sugars"], food["nf_protein"]))
@@ -102,6 +108,7 @@ def get_nutrition_facts():
 conn.commit()
 conn.close()
 
+<<<<<<< HEAD
 #combine meal + nutrition data and write it to a text file
 def process_data():
     conn = sqlite3.connect("meals.db")
@@ -117,3 +124,5 @@ def process_data():
     with open("output.txt", "w") as f:
         for r in results:
             f.write(f"{r[0]} | Rating: {r[1]} | Calories: {r[2]} | Fat: {r[3]}g | Sugar: {r[4]}g | Protein: {r[5]}g\n")
+=======
+>>>>>>> 70c3813 (minor updates + comments)
