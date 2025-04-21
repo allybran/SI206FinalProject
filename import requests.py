f5839f5031342e95c8291fd13b58d8ab48d05b86
Ally Brangham <allybrangham@Allys-MacBook-Air-4.local>
2025-04-20 21:36:41 -0400

    connected database

--- import requests.py
+++ import requests.py
@@ -1,6 +1,12 @@
-import requests
+#SI 206 Final Project
+#Lauren Starr and Ally Brangham
+#Group Name: Ann Arbor Foodies
+
+
+
+import sqlite3
 import json 
-import time 
+import requests
 
 # I made an account to download a free API key from Nutritionix
 #Below are my personal credentials 
@@ -8,15 +14,24 @@ import time
 app_id = "7b857982"
 app_key = "147987eb843d733fd4bd746545c15e0d"
 
+#connects to the database
+def connect_db():
+    conn = sqlite3.connect("meals.db")
+    cur = conn.cursor()
+    return conn, cur
+
 #searching nutrition info for each popular meal 
 def search_food(food_item): 
-    url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
-
+    conn, cur = connect_db()
+    cur.execute("SELECT id, name FROM meals")
+    meals = cur.fetchall()
+    
     headers = {
         "x-app-id": app_id,
         "x-app-key": app_key,
         "Content-Type": "application/json",
     }
+    url = "https://trackapi.nutritionix.com/v2/natural/nutrients" 
 
     data = {
         "query": food_item
@@ -26,8 +41,10 @@ def search_food(food_item):
 
     if response.status_code == 200: #status code 200 means the request worked
         result = response.json()
+
+
         if result["foods"]: 
-            food = result["food"] #this is jgoing to accept the first match of the search
+            food = result["foods"][0] #this is going to accept the first match of the search
             #print nutrition info
             print(f"Nutrition info for: {food['food_name'].title()}")
             print(f"Calories: {food['nf_calories']} kcal")
