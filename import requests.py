04bb95105df35b1111b224256f75fd9e05fae4cf
Ally Brangham <allybrangham@Allys-MacBook-Air-4.local>
2025-04-20 21:44:31 -0400

    food search

--- import requests.py
+++ import requests.py
@@ -5,11 +5,14 @@
 
 
 import sqlite3
-import json 
+import json
 import requests
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# I made an account to download a free API key from Nutritionix
-#Below are my personal credentials 
+# We made an account to download a free API key from Nutritionix
+#Below are our personal credentials 
 
 app_id = "7b857982"
 app_key = "147987eb843d733fd4bd746545c15e0d"
@@ -20,32 +23,56 @@ def connect_db():
     cur = conn.cursor()
     return conn, cur
 
+# create meals and nutrition tables
+def create_tables():
+    conn, cur = connect_db()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Meals (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            restaurant TEXT,
+            rating REAL
+        )
+    """)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Nutrition (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            meal_id INTEGER,
+            calories INTEGER,
+            fat_g REAL,
+            sugar_g REAL,
+            protein_g REAL,
+            FOREIGN KEY(meal_id) REFERENCES Meals(id)
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+
 #searching nutrition info for each popular meal 
-def search_food(food_item): 
+def get_nutrition_facts(): 
     conn, cur = connect_db()
     cur.execute("SELECT id, name FROM meals")
     meals = cur.fetchall()
-    
+
     headers = {
         "x-app-id": app_id,
         "x-app-key": app_key,
         "Content-Type": "application/json",
     }
     url = "https://trackapi.nutritionix.com/v2/natural/nutrients" 
-
-    data = {
-        "query": food_item
-    }
-
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code == 200: #status code 200 means the request worked
-        result = response.json()
+    count = 0
+    
+    for meal_id, food_item in meals: 
+        try: 
+            response =requests.post(url, headers=headers, json={"query":food_item})
+            food = response.json()["foods"][0] #this is going to accept the first match of the search
+            
+    
 
 
         if result["foods"]: 
-            food = result["foods"][0] #this is going to accept the first match of the search
-            #print nutrition info
+            food = result["foods"][0] 
             print(f"Nutrition info for: {food['food_name'].title()}")
             print(f"Calories: {food['nf_calories']} kcal")
             print(f"Total fat: {food['nf_total_fat']} grams")
